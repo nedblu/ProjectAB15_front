905a2bea1c96b6ee9ce040d7e21c192973463725
Carlos Aguilar <carlosaguilarnet@gmail.com>
2015-08-10 23:27:30 -0500

    Initial commit with structure

---
+++ public_html/index.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| First we need to get an application instance. This creates an instance
+| of the application / container and bootstraps the application so it
+| is ready to receive HTTP / Console requests from the environment.
+|
+*/
+
+$app = require __DIR__.'/../lumen/bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+// This only run with php artisan serve
+// $app->run();
+
+// This works good with apache
+$request = Illuminate\Http\Request::capture();
+$app->run($request);
