99ed4e4b0bf4332271cc696c67b4c6fdcaca501f
perezCamargo <perez.camargo7@gmail.com>
2015-08-15 01:19:25 -0500

    Adding Blade to Lumen project
    This changes is related to add Blade features into lumen.
    Steps below:
        *Run the comand: composer require vluzrmos/collective-html
        *Then Update composer running the command: composer update
        *In your bootstrap/app.php file put the following lines:
            -> $app->register('Collective\Html\HtmlServiceProvider');
            -> class_alias('Collective\Html\HtmlFacade', 'Html');
            -> class_alias('Collective\Html\FormFacade', 'Form');
        *In the same file (bootstrap/app.php) uncoment the line: $app->withFacades()
        *The las thing is just verify the composer.json file and check that the following lines are there in "require:{}":
            -> "vluzrmos/collective-html": "1.0.9 as 5.1",
               "package-vendor/package-required-name":"package-version"
    
    For more information visit the link: https://github.com/vluzrmos/collective-html
    
    @aguilarcarlos done the manual about how to add blade in Lumen projects :3

---
+++ lumen/resources/views/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+	<nav>
+        <ul>
+            <li id="logo"><img src="img/ABlogo.png"></li>
+            <li id="special-menu" class="menu-link"><a href="index.html"><img src="img/ABinicio.png" class="menu-image"></a></li>
+            <li class="menu-link"><a href="acerca.html"><img src="img/ABacerca.png" class="menu-image"></a></li>
+            <li class="menu-link"><a href="catalogo.html"><img src="img/ABcatalogo.png"class="menu-image"></a></li>
+            <li class="menu-link"><a href="tecnicas.html"><img src="img/ABtecnicas.png" class="menu-item-special"></a></li>
+            <li class="menu-link"><a href="sucursales.html"><img src="img/ABsucursales.png" class="menu-item-special"></a></li>
+            <li class="menu-link"><a href="contacto.html"><img src="img/ABcontacto.png" class="menu-image"></a></li>
+        </ul>
+    </nav>
+</header>
