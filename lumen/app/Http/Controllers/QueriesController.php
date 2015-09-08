bb8a45cd265dc7d0ec2ce0d7335f2d2c23804c04
perezCamargo <perez.camargo7@gmail.com>
2015-09-07 23:12:05 -0500

    Product View
    Adding the dummy structure of the view.
    Showing real data.

--- lumen/app/Http/Controllers/QueriesController.php
+++ lumen/app/Http/Controllers/QueriesController.php
@@ -34,25 +34,25 @@ class QueriesController extends Controller {
         $title = $title[0]->name;
         foreach ($products as $product) {
             $parentName = DB::table('categories')->where('id',$product->parent_id)->get();
-            $parentName = $parentName[0]->name;
-            $product->parent_id = $parentName;
+            $product->parent_id = $parentName[0]->name;
         }
 
         return view('catalogo.productos', ['title' => $title, 'products' => $products]);
     }
 
-    //nombre,imagen,descricion,sku,colores,ink,equipment,stock
-    //description,colores,ink,equipment
-
+    //nombre,imagen,descripcion,sku,colores,ink,equipment,stock
     public function getItem($product_id){
         $product = DB::table('products')->where('id', $product_id)->get();
 
         $description = DB::table('descriptions')->where('id', $product[0]->description_id)->get();
         $product[0]->description_id = $description[0]->body;
 
+        $parentName = DB::table('categories')->where('id', $product[0]->parent_id)->get();
+        $product[0]->parent_id = $parentName[0]->name;
+
         if ($product[0]->colors) {
             $color_list = DB::table('product_colors')->select('colors_ar')->where('product_id', $product_id)->get();
-            $colores = $this->getColorsOfProduct($color_list[0]->colors_ar);
+            $colors = $this->getColorsOfProduct($color_list[0]->colors_ar);
         }
 
         if ($product[0]->ink) {
@@ -64,23 +64,11 @@ class QueriesController extends Controller {
             $equipment = DB::table('product_equips')->where('product_id', $product_id)->get();
             $product[0]->equipment = $equipment[0]->equip_ar;
         }
-        
-        // echo '<pre> PRODUCTO:::::::::::::::<br>';
-        // var_dump($product);
-        // echo '<br>';
-        // echo '<br>COLORES:::::::::::::::::<br>';
-        // var_dump($colores);
-        // echo '<br>';
-        // echo '<br>DETAILS:::::::::::::::::<br>';
-        // var_dump($description);
-        // echo '<br>';
-        // echo '<br>INKS:::::::::::::::::<br>';
-        // var_dump($ink);
-        // echo '<br>';
-        // echo '<br>EQUIPMENT:::::::::::::::::<br>';
-        // var_dump($equipment);  
-
-        return view('catalogo.item', ['title' => $product[0]->name, 'product' => $product]);
+        if ($product[0]->colors) {
+            return view('catalogo.item', ['title' => $product[0]->name, 'product' => $product, 'colors' => $colors]);
+        }else{
+            return view('catalogo.item', ['title' => $product[0]->name, 'product' => $product]);
+        }
     }
 
 }
--- lumen/resources/views/catalogo/item.blade.php
+++ lumen/resources/views/catalogo/item.blade.php
@@ -8,14 +8,44 @@
                 @include('default.title')
                 <section id="catalogo">
                         <div class="producto">
-                            <h4>{!! $title !!}</h4>
-                            {!! Html::image('img/no.png') !!} 
+                            <h4>{!! $title !!} <span class="sku">({!! $product[0]->sku !!})</span></h4>
+
+                            @if ($product[0]->image == 'na.png')
+                                {!! Html::image('img/no.png', 'image',['class' => 'mainImage']) !!} 
+                                 <br/>
+                            @else
+                                {!! Html::image('img/'.$product[0]->image,'image',['class' => 'mainImage']) !!} 
+                                 <br/>
+                            @endif
+                            
+                            @if ($product[0]->ink != 0)
+                                <span class="ink">TINTA: {!! $product[0]->ink !!}</span>
+                                 <br/>
+                            @endif
+
+                            @if ($product[0]->equipment != 0)
+                                <span class="ink">EQUIPO: {!! $product[0]->equipment !!}</span>
+                                 <br/>
+                            @endif
+
+                            @if ($product[0]->colors)
+                                @foreach ($colors as $color)
+                                    {!! Html::image('content/colors/'.$color->image, $color->name, ['class' => 'color']) !!}
+                                @endforeach
+                                 <br/>
+                            @endif
+                            <p>
+                                {!! $product[0]->description_id !!}
+                            </p>
+
                         </div>          
                 </section>
 
             <?php
+            //nombre,imagen,descripcion,sku,colores,ink,equipment,stock
             echo '<pre> PRODUCTO:::::::::::::::<br>';
             var_dump($product);
+            echo $product[0]->ink;
             echo "</pre>";
             ?>
                 @include('default.footer')
