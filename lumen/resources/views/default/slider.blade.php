4b5920f0d84716041f623442cda4af69ddc890a0
Carlos Aguilar <carlosaguilarnet@gmail.com>
2015-10-12 00:30:31 -0500

    Minor changes in slider, image deleted

--- lumen/resources/views/default/slider.blade.php
+++ lumen/resources/views/default/slider.blade.php
@@ -5,11 +5,11 @@
 
 		@foreach($banners as $slide)
 			<li>
-				@if($slide->uri && $slide->uri != "uri")
-					<a href="{!! url( $slide->uri ) !!}" title="{!! $slide->title !!}" >{!! Html::image('content/slide-show/caption_' . $slide->image, $slide->title) !!}</a>
+				@if($slide->uri != null && $slide->uri != "uri")
+					<a href="{!! url( $slide->uri ) !!}" title="{!! $slide->title !!}" >{!! Html::image('content/slide-show/' . $slide->image, $slide->title) !!}</a>
 					<p class="flex-caption">{!! $slide->title !!}</p>
 				@else
-					{!! Html::image('content/slide-show/caption_' . $slide->image, $slide->title) !!}
+					{!! Html::image('content/slide-show/' . $slide->image, $slide->title) !!}
 					<p class="flex-caption">{!! $slide->title !!}</p>
 				@endif
 				
